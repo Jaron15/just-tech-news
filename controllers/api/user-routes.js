9728bc0b1cb3253f658930203cf6e0cab539716a
Jaron <jjkenyon777@gmail.com>
2021-10-24 18:53:46 -0700

    .save error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,7 +63,16 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        console.log(req.session)
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -88,8 +97,15 @@ User.findOne({
         res.status(400).json({message: 'Incorrect password!'});
         return; 
     }
+    console.log(req)
+    req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbuserData.username;
+        req.session.loggedIn = true;
 
-    res.json({ user: dbUserData, message: 'You are now logged in!'});
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
 });
 });
 // PUT /api/users/1
